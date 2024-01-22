d1c3fe28a2dca6ab0ca94e457aac4bbf5666c756
Holms2009 <holms200990@gmail.com>
2024-01-22 16:58:10 +0300

    Some logics for sidebar

--- src/components/SideBar/config.js
+++ src/components/SideBar/config.js
@@ -3,7 +3,7 @@ import userIcon from '../../assets/images/user.svg';
 import serversIcon from '../../assets/images/servers.svg';
 import dataBasesIcon from '../../assets/images/data_bases.svg';
 import ipAdressesIcon from '../../assets/images/ip_adresses.svg';
-import balansersIcon from '../../assets/images/balansers.svg';
+import balancersIcon from '../../assets/images/balansers.svg';
 import backupsIcon from '../../assets/images/backups.svg';
 import monitoringIcon from '../../assets/images/monitoring.svg';
 import domensIcon from '../../assets/images/domens.svg';
@@ -15,25 +15,25 @@ import ideaIcon from '../../assets/images/idea.svg';
 
 export const menu = [
   [
-    { name: 'Заказать VPS', icon: cartIcon, submenu: null },
-    { name: 'Аккаунт', icon: userIcon, submenu: [] }
+    { id: 'vps', name: 'Заказать VPS', icon: cartIcon, submenu: null },
+    { id: 'account', name: 'Аккаунт', icon: userIcon, submenu: [] }
   ],
   [
-    { name: 'Серверы', icon: serversIcon, submenu: [] },
-    { name: 'Базы данных', icon: dataBasesIcon, submenu: null },
-    { name: 'IP-адреса', icon: ipAdressesIcon, submenu: null },
-    { name: 'Балансировщики', icon: balansersIcon, submenu: null },
-    { name: 'Облачные бэкапы', icon: backupsIcon, submenu: null }
+    { id: 'servers', name: 'Серверы', icon: serversIcon, submenu: [] },
+    { id: 'database', name: 'Базы данных', icon: dataBasesIcon, submenu: null },
+    { id: 'ip_addresses', name: 'IP-адреса', icon: ipAdressesIcon, submenu: null },
+    { id: 'balancers', name: 'Балансировщики', icon: balancersIcon, submenu: null },
+    { id: 'backups', name: 'Облачные бэкапы', icon: backupsIcon, submenu: null }
   ],
   [
-    { name: 'Мониторинг', icon: monitoringIcon, submenu: null },
-    { name: 'Домены', icon: domensIcon, submenu: [] },
-    { name: 'SSL', icon: sslIcon, submenu: null },
-    { name: 'Магазин', icon: shopIcon, submenu: null },
-    { name: 'SEO и реклама', icon: seoIcon, submenu: null }
+    { id: 'monitoring', name: 'Мониторинг', icon: monitoringIcon, submenu: null },
+    { id: 'domains', name: 'Домены', icon: domensIcon, submenu: [] },
+    { id: 'ssl', name: 'SSL', icon: sslIcon, submenu: null },
+    { id: 'shop', name: 'Магазин', icon: shopIcon, submenu: null },
+    { id: 'seo', name: 'SEO и реклама', icon: seoIcon, submenu: null }
   ],
   [
-    { name: 'Поддержка', icon: supportIcon, submenu: null },
-    { name: 'Есть идея', icon: ideaIcon, submenu: null },
+    { id: 'support', name: 'Поддержка', icon: supportIcon, submenu: null },
+    { id: 'idea', name: 'Есть идея', icon: ideaIcon, submenu: null },
   ],
 ]
--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames';
+import { useState } from 'react';
 
 import styles from './SideBar.module.css';
 
@@ -6,13 +7,23 @@ import { menu } from './config';
 import { ThemeToggle } from '../ThemeToggle';
 
 export function SideBar({ isOpened }) {
+  const [activeItem, setActiveItem] = useState('vps');
+
+  function menuItemClickHandler(id, hasSubgroup) {
+    setActiveItem(id);
+  }
+
   return (
     <aside className={classNames(styles.wrapper, { [styles.opened]: isOpened })}>
       <div className={styles.navMenu}>
         {menu.map((group, index) => (
           <div className={styles.menuGroup} key={index}>
             {group.map((item) => (
-              <div className={styles.menuItem} key={item.name}>
+              <div
+                className={classNames(styles.menuItem, { [styles.active]: activeItem === item.id })}
+                onClick={() => menuItemClickHandler(item.id)}
+                key={item.id}
+              >
                 <img className={styles.menuIcon} src={item.icon} alt="" />
                 <span className={styles.menuText}>{item.name}</span>
               </div>
