7f79fd1b68ddf6cf2419ee6e374f256dbcb01b76
Holms2009 <holms200990@gmail.com>
2024-01-22 20:12:20 +0300

    Making cards

--- src/components/PlanCard/index.jsx
+++ src/components/PlanCard/index.jsx
@@ -8,6 +8,21 @@ export function PlanCard({data}) {
         <img src={getCategoryIcon(data.category)} alt="" />
         <span className={styles.planName}>{data.name}</span>
       </div>
+      <p className={styles.price}>{`${data.price_per_month} ₽/мес`}</p>
+      <div className={styles.specs}>
+        <div className={styles.specsItem}>
+          <span className={styles.caption}>CPU</span>
+          <span className={styles.specsValue}>{`${data.cpu_cores} x 2.8 ГГц`}</span>
+        </div>
+        <div className={styles.specsItem}>
+          <span className={styles.caption}>RAM</span>
+          <span className={styles.specsValue}>{`${data.ram} МБ`}</span>
+        </div>
+        <div className={styles.specsItem}>
+          <span className={styles.caption}>DISK</span>
+          <span className={styles.specsValue}>{`${data.volume_disk} ${data.disk_type}`}</span>
+        </div>
+      </div>
     </div>
   )
 }
